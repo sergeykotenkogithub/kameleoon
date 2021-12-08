2df209f4fe0cd0ce97f600feb3713e89518767eb
sergeykotenko <sergeykotenko@bk.ru>
2021-12-08 18:34:52 +0300

    search

--- js/my.js
+++ js/my.js
@@ -13,41 +13,37 @@ document.querySelectorAll(".clipboard-img").forEach(selector => {
 
 const input = document.querySelector('.search-text')
 
-// input.addEventListener('input', element => {
-//     const val = input.value;
-//     const h1 = document.querySelectorAll('h1');
-//     if (val !== '') {
-//         document.querySelector('.show-search').style.display = 'block'
-//         h1.forEach(selector =>  {
-//             if(selector.innerText.search(val) == -1) {
-//                 console.log('wwww')
-//             } else {
-//                 // console.log(selector.innerText)
-//                 console.log(selector)
-//             }
-//         })
-//     }
-// })
-
 input.addEventListener('input', element => {
     const val = input.value;
-    const h1 = document.querySelectorAll('h3 a');
+    const h3 = document.querySelectorAll('h3 a');
     if (val !== '') {
+        document.querySelector('.show-search').innerHTML = '';
         document.querySelector('.show-search').style.display = 'block'
-        h1.forEach(selector =>  {
+        let aaa = [];
+        h3.forEach(selector =>  {
             if(selector.innerText.search(val) == -1) {
-                // console.log('wwww')
+               // const bbb =  document.querySelectorAll('.search-block')
+               //  for (let i = 0; i < bbb.length; i++) {
+               //      bbb[i].remove();
+               //  }
             } else {
-                // console.log(selector.innerText)
                 console.log(selector.innerText)
-                // document.querySelector('.show-search').insertAdjacentElement('beforeend', )
+                const clone = selector.cloneNode(true)
+                // for (let i=0, len=array.length; i<len; i++)
+                if (aaa.indexOf(clone) === -1) {
+                    aaa.push(clone)
+                }
+                console.log(aaa)
 
+                aaa.forEach(e => {
+                    e.classList.add('search-block')
+                    document.querySelector('.show-search').insertAdjacentElement('afterbegin', e)
+                })
             }
         })
     } else  {
         document.querySelector('.show-search').style.display = 'none'
     }
-
 })
 
 
