0d002333a1d5f3bb5b3f2e668d44c557a797eb9b
sergeykotenko <sergeykotenko@bk.ru>
2021-12-09 12:53:50 +0300

    search h1 and h3

--- js/my.js
+++ js/my.js
@@ -13,61 +13,38 @@ document.querySelectorAll(".clipboard-img").forEach(selector => {
 
 const input = document.querySelector('.search-text')
 
-input.addEventListener('input', element => {
+input.addEventListener('input', () => {
+
     const val = input.value;
     const h3 = document.querySelectorAll('h3 a');
-    const h1 = document.querySelectorAll('h1');
+    const h1 = document.querySelectorAll('h1 a');
+
     if (val !== '') {
+
         document.querySelector('.show-search').innerHTML = '';
         document.querySelector('.show-search').style.display = 'block'
         let cloneSelectors = [];
-        let cloneSelectors2 = [];
-        h3.forEach(selector =>  {
-            if(selector.innerText.toLowerCase().search(val.toLowerCase()) !== -1) {
-                console.log(selector.innerText)
-                const clone = selector.cloneNode(true)
-                if (cloneSelectors.indexOf(clone) === -1) {
-                    cloneSelectors.push(clone)
+        function elementAdd(element) {
+            element.forEach(selector =>  {
+                if(selector.innerText.toLowerCase().search(val.toLowerCase()) !== -1) {
+                    const clone = selector.cloneNode(true)
+                    if (cloneSelectors.indexOf(clone) === -1) {
+                        cloneSelectors.push(clone)
+                    }
+                    cloneSelectors.forEach(el => {
+                        el.classList.add('search-block')
+                        document.querySelector('.show-search').insertAdjacentElement('afterbegin', el)
+                    })
                 }
-                cloneSelectors.forEach(el => {
-                    el.classList.add('search-block')
-                    document.querySelector('.show-search').insertAdjacentElement('afterbegin', el)
-                })
-            }
-        })
-        // h1.forEach(selector =>  {
-        //     if(selector.innerText.toLowerCase().search(val.toLowerCase()) !== -1) {
-        //         console.log(selector.innerText)
-        //         const clone = selector.cloneNode(true)
-        //         // for (let i=0, len=array.length; i<len; i++)
-        //         // if (cloneSelectors.indexOf(clone) === -1) {
-        //         //     cloneSelectors.push(clone)
-        //         // }
-        //         // if (cloneSelectors.length > 0) {
-        //         //     for (let cl of cloneSelectors) {
-        //         //         if (!cl.includes(clone)) {
-        //         //             cl.push(clone);
-        //         //         }
-        //         //     }
-        //         // }
-        //         if (cloneSelectors.indexOf(clone) === -1) {
-        //                         cloneSelectors.push(clone)
-        //         }
-        //
-        //         // cloneSelectors.push(clone)
-        //
-        //         // if (cloneSelectors.indexOf(clone) === -1) {
-        //         //     cloneSelectors.push(clone)
-        //         // }
-        //         console.log(cloneSelectors)
-        //         cloneSelectors.forEach(el => {
-        //             el.classList.add('search-block')
-        //             document.querySelector('.show-search').insertAdjacentElement('afterbegin', el)
-        //         })
-        //     }
-        // })
+            })
+        }
+        elementAdd(h3)
+        elementAdd(h1)
+
     } else  {
+
         document.querySelector('.show-search').style.display = 'none'
+
     }
 })
 
